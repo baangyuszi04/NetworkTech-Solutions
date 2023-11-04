8dcc4f0521bef5553a4f4985550f81f8da4d78d9
Gyusz119 <gyugyu5000@gmail.com>
2023-11-04 11:48:10 +0100

    rólunk, kapcsolat oldal

---
+++ default/views.py
@@ -0,0 +1,48 @@
+"""
+    View functions for the core pages
+    @package networktechsolutions
+    @author Noel Nagy
+    @website: https://github.com/nagynooel
+    ©2023 Noel Nagy - All rights reserved.
+"""
+from django.shortcuts import render
+
+# Index page
+def index_view(request):
+    return render(request, "default/index.html")
+
+
+def about_us_view(request):
+    return render(request, "default/rolunk.html")
+
+
+def contact_view(request):
+    return render(request, "default/kapcsolat.html")
+
+
+def shop_view(request):
+    return render(request, "default/aruhaz.html")
+
+
+def product_view(request, product_name):
+    context = {
+        "product_name": product_name
+    }
+    
+    return render(request, "default/termek.html", context=context)
+
+
+def cart_view(request):
+    return render(request, "default/kosar.html")
+
+
+def payment_view(request):
+    return render(request, "default/fizetes.html")
+
+
+def order_status_view(request, order_number):
+    context = {
+        "order_number": order_number
+    }
+
+    return render(request, "default/rendeles-allapota.html", context=context)
